f20d9b52e827a2adba932bb18ad336ed5c7ba078
MykyBalb <mykaelbalbino06@gmail.com>
2025-03-19 22:02:16 -0300

    Atividade 04

---
+++ atividad iot-20250320T000639Z-001/atividad iot/ifaci/aula04/Atividade04/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Atividade04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkGray;
+            this.button2.Location = new System.Drawing.Point(737, 58);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 53);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "DESLIGAR";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Cyan;
+            this.button3.Location = new System.Drawing.Point(591, 237);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 53);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "CALCULADORA";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Red;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(591, 325);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(280, 121);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "EMERGÊNCIA";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Cyan;
+            this.button6.Location = new System.Drawing.Point(737, 237);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(137, 53);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "BLOCO DE NOTAS";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.DarkGray;
+            this.button4.Location = new System.Drawing.Point(670, 497);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(137, 53);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "SAIR";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(10, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(383, 556);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Cyan;
+            this.button7.Location = new System.Drawing.Point(591, 151);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(137, 53);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "ENCHER TANQUE";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Cyan;
+            this.button8.Location = new System.Drawing.Point(734, 151);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(137, 53);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "ESVAZIAR TANQUE";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(399, 29);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(156, 238);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(665, 553);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 33);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(594, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "LIGAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 2000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 2000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 606);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Atividade04";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ atividad iot-20250320T000639Z-001/atividad iot/ifaci/aula04/Atividade04/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade04
+{
+    public partial class Form1 : Form
+    {
+        // Lista para armazenar as imagens
+        private List<Image> imagens1;
+        private List<Image> imagens2;
+        private int imagemAtual1;
+        private int imagemAtual2;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            imagens1 = new List<Image>
+            {
+                Image.FromFile("c:\\imagens\\TanqueEnchendo1.bmp"),
+                Image.FromFile("c:\\imagens\\TanqueEnchendo2.bmp"),
+                Image.FromFile("c:\\imagens\\TanqueEnchendo3.bmp"),
+                Image.FromFile("c:\\imagens\\TanqueEnchendo4.bmp")
+            };
+           
+            imagens2 = new List<Image>
+            {
+                Image.FromFile("c:\\imagens\\TanqueEsvaziando1.bmp"),
+                Image.FromFile("c:\\imagens\\TanqueEsvaziando2.bmp"),
+                Image.FromFile("c:\\imagens\\TanqueEsvaziando3.bmp"),
+                Image.FromFile("c:\\imagens\\TanqueEsvaziando4.bmp"),
+                Image.FromFile("c:\\imagens\\TanqueVazio.bmp")
+            };
+
+            pictureBox1.Image = Image.FromFile("c:\\imagens\\TanqueCheio.bmp");
+            imagemAtual2 = 0; 
+             pictureBox1.Image = Image.FromFile("c:\\imagens\\TanqueVazio.bmp");
+            imagemAtual1 = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        
+        private void button7_Click(object sender, EventArgs e)
+        {
+            timer2.Start(); 
+        }
+
+        
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+           
+            pictureBox1.Image = imagens1[imagemAtual1];
+
+           
+            imagemAtual1++;
+
+            
+            if (imagemAtual1 >= imagens1.Count)
+            {
+                timer2.Stop(); 
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            timer3.Start();
+
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+
+            pictureBox1.Image = imagens2[imagemAtual2];
+
+
+            imagemAtual2++;
+
+
+            if (imagemAtual2 >= imagens2.Count)
+            {
+                timer3.Stop();
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timer2.Stop();
+            timer3.Stop();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
